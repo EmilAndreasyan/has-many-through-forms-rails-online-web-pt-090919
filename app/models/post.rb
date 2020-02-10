50e27986abfb04f9c2ef8f456c1927451451cc1a
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-02-10 07:46:32 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,3 +12,11 @@ class Post < ActiveRecord::Base
   end
 
 end
+
+
+def categories_attributes=(category_attributes)
+  category_attributes.values.each do |category_attribute|
+    category = Category.find_or_create_by(category_attribute)
+    self.post_categories.build(category: category)
+  end
+end
